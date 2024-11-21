818bca4dec7faaf4cf86bfab3650c709c973fa1e
fillah23 <fillahseptian7l24@gmail.com>
2024-11-21 17:47:30 +0700

    v2

--- API/faq.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$queryResult=$db->query("SELECT * FROM faq");
-$result=array();
-while($fetchData=$queryResult->fetch_assoc()){
-    $result[]=$fetchData;
-}
-echo json_encode($result);
-?>
--- API/produk.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$queryResult=$db->query("SELECT * FROM produk");
-$result=array();
-while($fetchData=$queryResult->fetch_assoc()){
-    $result[]=$fetchData;
-}
-echo json_encode($result);
-?>
